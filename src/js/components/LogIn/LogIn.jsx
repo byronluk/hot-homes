ae695fe952e419c0db35597233682b86111147e9
Brett Schalin <brettschalin@gmail.com>
2018-03-01 15:22:24 -0800

    changed login check logic

--- src/js/components/LogIn/LogIn.jsx
+++ src/js/components/LogIn/LogIn.jsx
@@ -12,72 +12,70 @@ class LogIn extends React.Component {
     const { email, password } = this.props.login;
     e.preventDefault();
 
-    //literally black magic
-    if (!email.match(/^[^@]+@[^\.]+(\.[a-z0-9]+)*\.[a-z]+/i)) {
-      console.log('email validation failed');
-      return false;
-    }
-
     if (password.length < 8) {
       console.log('password validation failed');
       return false;
     }
-     axios.get('./api/users').then( data => {
-       if (this.isMatching (data.data, email, password)) {
-         dispatch(logIn({ email: email, password: password }))
-       } else {
-         console.log('Invalid email or password')
-       }
-       
-     })
+    axios.get('./api/users').then(data => {
+      if (this.isMatching(data.data, email, password)) {
+        dispatch(logIn({ email: email, password: password }))
+      } else {
+        console.log('Invalid email or password')
+      }
+
+    })
   }
-isMatching(data, name) {
-  console.log(data);
-  data.forEach( item => {});
-    (entry => {
-      if (entry.email == email && entry.password == password ) {
+
+  isMatching(data, name, password) {
+    console.log(data);
+
+    for (let i = 0; i < data.length; i++) {
+      let entry = data[i];
+      if ((entry.email == name || entry.username == name) && entry.password == password) {
         return true;
       }
-    });
+    }
     return false;
+  }
+
+
+updateForm(e) {
+  const { dispatch } = this.props;
+  const name = e.target.name;
+  const value = e.target.value;
+
+  dispatch(updateLogIn({ name: name, value: value }));
 }
-  updateForm(e) {
-    const { dispatch } = this.props;
-    const name = e.target.name;
-    const value = e.target.value;
-    
-    dispatch(updateLogIn({ name: name, value: value }));
-  }
 
-  render() {
-    const { logIn } = this.props;
-    return (
-      <div>
-        <form className='Login-form'>
-          <input
-            type='text'
-            onChange={this.updateForm}
-            name='email'
-            placeholder='Email' />
+render() {
+  const { logIn } = this.props;
+  return (
+    <div>
+      <form className='Login-form'>
+        <input
+          type='text'
+          onChange={this.updateForm}
+          name='email'
+          placeholder='Email' />
 
-          <input
-            type='password'
-            onChange={this.updateForm}
-            name='password'
-            placeholder='Password' />
+        <input
+          type='password'
+          onChange={this.updateForm}
+          name='password'
+          placeholder='Password' />
 
-          <input
-            type='checkbox'
-            onChange={this.updateForm}
-            name='remember' />Remember me
+        <input
+          type='checkbox'
+          onChange={this.updateForm}
+          name='remember' />Remember me
           <button
-            type='submit'
-            onClick={this.validateAndSubmit}>Log In</button>
-        </form>
-        <p>Forgot your password?</p>
-      </div>
-    )
-  }
+          type='submit'
+          onClick={this.validateAndSubmit}>Log In</button>
+      </form>
+      <p>Forgot your password?</p>
+    </div>
+  )
+}
 }
 
 export default LogIn;
