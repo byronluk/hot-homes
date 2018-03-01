459454c1c426dae6a5e6ab581e3200cf6febb1de
Byron Luk <byronluk@gmail.com>
2018-03-01 14:30:55 -0800

    debugged merged files

--- src/js/App.jsx
+++ src/js/App.jsx
@@ -5,7 +5,7 @@ import history from './history';
 import Home from './views/HomePage';
 import Listings from './views/ListingsPage';
 import RentListings from './views/RentListingsPage';
-import ListingsDetail from './views/ListingsDetailPage';
+import ListingsDetails from './views/ListingsDetailsPage';
 import PublishListings from './views/PublishListingsPage';
 // import Profile from './views/ProfilePage';
 
@@ -15,8 +15,8 @@ export default class App extends React.Component {
       <Router history={ history }>
         <div className='container'>
           <Route exact path='/' component={Home} />
-          <Route path='/listings' component={Listings} />
-          <Route path='/listings/:id' component={ListingsDetail} />
+          <Route exact path='/listings' component={Listings} />
+          <Route path='/listings/:id' component={ListingsDetails} />
           <Route path='/listings/:id/:book' component={RentListings} />
           {/* <Route path='/user' component={Profile} /> */}
           <Route path='/user/publish' component={PublishListings} />
--- src/js/views/ListingsDetailsPage/ListingDetails.jsx
+++ src/js/views/ListingsDetailsPage/ListingDetails.jsx
@@ -1,60 +1,61 @@
 import React from 'react';
 import axios from 'axios';
 import RentListings from '../RentListingsPage';
+import NavigationBar from '../../components/NavigationBar';
 
 export default class ListingDetails extends React.Component {
 
-    constructor(props) {
-        super(props);
+  constructor(props) {
+    super(props);
 
-        this.loading = 'loading...';
+    this.loading = 'loading...';
 
-        this.state = {
-            description: this.loading,
-            address: this.loading,
-        }
+    this.state = {
+      description: this.loading,
+      address: this.loading,
     }
+  }
 
-    componentDidMount() {
-        console.log(this.props.location);
-        const id = this.props.location.pathname.substr(10);
-        axios.get(`/api/properties/${id}`)
-            .then(response => {
-                this.setState({ ...response.data, error: null });
-            })
-            .catch(err => {
-                console.log('Something went wrong: ' + err);
-                this.setState({
-                    error: err,
-                });
-            })
+  componentDidMount() {
+    const id = this.props.location.pathname.substr(10);
+    axios.get(`/api/properties/${id}`)
+      .then(response => {
+        this.setState({ ...response.data, error: null });
+      })
+      .catch(err => {
+        console.log('Something went wrong: ' + err);
+        this.setState({
+          error: err,
+        });
+      })
+  }
+
+
+
+  render() {
+    if (!this.state.error) {
+
+      return (
+        <div>
+          <NavigationBar />
+          <div>
+            <p>{this.state.address.city}, {this.state.address.state}</p>
+            <p>{this.state.description.bedrooms} bedrooms, {this.state.description.bathrooms} bathrooms</p>
+            <p>Price: ${this.state.description.price}</p>
+            <p>{this.state.description.description}</p>
+          </div>
+
+          <RentListings />
+        </div>
+
+      )
     }
-
-
-
-    render() {
-        if (!this.state.error) {
-
-            return (
-                <div>
-                    <div>
-                        <p>{this.state.address.city}, {this.state.address.state}</p>
-                        <p>{this.state.description.bedrooms} bedrooms, {this.state.description.bathrooms} bathrooms</p>
-                        <p>Price: ${this.state.description.price}</p>
-                        <p>{this.state.description.description}</p>
-                    </div>
-
-                    <RentListings />
-                </div>
-
-            )
-        }
-        return (
-            <div>
-                <h3>Error! Something went wrong on our end. More details below</h3>
-                <p>{this.state.error}</p>
-            </div>
-        )
-    }
+    return (
+      <div>
+        <h3>Error! Something went wrong on our end. More details below</h3>
+        <p>{this.state.error}</p>
+      </div>
+    )
+  }
 
 }
