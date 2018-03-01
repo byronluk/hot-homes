a485fb01e2870ca0a5013df2a06900ad7d3180eb
Ying Fan <yingcathyfan@gmail.com>
2018-03-01 09:55:44 -0800

    Change navigation action

--- src/js/actions/navigation-bar.js
+++ src/js/actions/navigation-bar.js
@@ -1,14 +1,6 @@
 export const toggleHidden = (value) => {
     return {
-<<<<<<< HEAD
       type: 'TOGGLE_HIDDEN',
       toggleLogIn: value
     }
   }
-=======
-      type: 'TOGGLE_HIDDEn',
-      togglehidden: value
-    }
-  }
-
->>>>>>> change in login page
