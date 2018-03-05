94cf23d5311d5f25303ebf3b6a9f3eeafce23c7c
Ying Fan <yingcathyfan@gmail.com>
2018-03-05 08:39:42 -0800

    small change in publishlistings page

--- src/js/views/PublishListingsPage/PublishListings.jsx
+++ src/js/views/PublishListingsPage/PublishListings.jsx
@@ -123,18 +123,19 @@ class PublishListings extends React.Component {
                 </div>
               </div>
             </form>
-            </div>
           </div>
+        </div>
         <Footer />
       </div>
-        );
-}}
-      
+    );
+  }
+}
+
 PublishListings.propTypes = {
-      publishListings: PropTypes.object,
-      updateForm: PropTypes.func,
-      handleSubmit: PropTypes.func,
-      handleCancel: PropTypes.func,
-    };
-    
-    export default PublishListings;
+  publishListings: PropTypes.object,
+  updateForm: PropTypes.func,
+  handleSubmit: PropTypes.func,
+  handleCancel: PropTypes.func,
+};
+
+export default PublishListings;
