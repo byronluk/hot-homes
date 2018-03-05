afec0d1a62aad5f551271be1b06421f13c7f3a01
Byron Luk <byronluk@gmail.com>
2018-03-05 10:43:28 -0800

    updated listing details page

--- src/js/views/ListingsDetailsPage/ListingDetails.jsx
+++ src/js/views/ListingsDetailsPage/ListingDetails.jsx
@@ -39,61 +39,59 @@ export default class ListingDetails extends React.Component {
       return (
         <div>
           <NavigationBar />
-          <div className="main-section">
-            {!!description.photoUrl &&
-              <Carousel>
-                {!!description.photoUrl.length &&
-                  description.photoUrl.map((photo, index) => {
-                    return (
-                      <div key={index}>
-                        <img src={photo} alt="property images" />
-                      </div>);
-                  })
-                }
-              </Carousel>}
-            <div className="grid">
-              <div className="property-details">
-                <p
-                  className="is-size-4 address-title"
-                >
-                  {`${address.street}, ${address.city} ${address.state}`}
-                </p>
-                <p>{`${description.description}`}</p>
-                <div className="tags main-info-tags">
-                  <span className="tag">{`${description.bedrooms} bedrooms`}</span>
-                  <span className="tag">{`${description.bathrooms} bathrooms`}</span>
-                  <span className="tag">{`$${description.price} per month`}</span>
-                </div>
-                <hr />
-                <p className="amenities-title">Amenities</p>
-                {!!description.amenities &&
-                  <div>
-                    {!!description.amenities.length &&
-                      <div className="tags amenity-tags">
-                        {description.amenities.map((amenity, index) => {
-                          return (
-                            <span className="tag is-primary" key={index}>{amenity}</span>
-                          );
-                        })}
-                      </div>
-                    }
+          {description.price &&
+            <div className="main-section">
+              {!!description.photoUrl &&
+                <Carousel>
+                  {!!description.photoUrl.length &&
+                    description.photoUrl.map((photo, index) => {
+                      return (
+                        <div key={index}>
+                          <img src={photo} alt="property images" />
+                        </div>);
+                    })
+                  }
+                </Carousel>}
+              <div className="grid">
+                <div className="property-details">
+                  <p
+                    className="is-size-4 address-title"
+                  >
+                    {`${address.street}, ${address.city} ${address.state}`}
+                  </p>
+                  <p>{`${description.description}`}</p>
+                  <div className="tags main-info-tags">
+                    <span className="tag">{`${description.bedrooms} bedrooms`}</span>
+                    <span className="tag">{`${description.bathrooms} bathrooms`}</span>
+                    <span className="tag">{`$${description.price} per month`}</span>
                   </div>
-                }
+                  <hr />
+                  <p className="amenities-title">Amenities</p>
+                  {!!description.amenities &&
+                    <div>
+                      {!!description.amenities.length &&
+                        <div className="tags amenity-tags">
+                          {description.amenities.map((amenity, index) => {
+                            return (
+                              <span className="tag is-primary" key={index}>{amenity}</span>
+                            );
+                          })}
+                        </div>
+                      }
+                    </div>
+                  }
+                </div>
+                <RentListings />
               </div>
-              <RentListings />
-              <Footer />
-            </div>
-            </div>
-            </div>
-
-            )
-      
-          }
-          return (
+            </div>}
+        </div >
+      );
+    }
+    return (
       <div>
-              <h3>Error! Something went wrong on our end. More details below</h3>
-              <p>{this.state.error}</p>
-            </div>
-            );
-          }
-        }
+        <h3>Error! Something went wrong on our end. More details below</h3>
+        <p>{this.state.error}</p>
+      </div>
+    );
+  }
+}
