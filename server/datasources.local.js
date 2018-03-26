c9d7f6c4c2797cd9b093c711c7293dce71d5803f
byronluk <byronluk@gmail.com>
2018-03-26 14:58:55 -0700

    added local and production environments

---
+++ server/datasources.local.js
@@ -0,0 +1,12 @@
+module.exports = {
+  MongoDB: {
+    host: '',
+    port: 0,
+    url: '',
+    database: 'hothomes',
+    password: '',
+    name: 'MongoDB',
+    user: '',
+    connector: 'mongodb'
+  }
+};
