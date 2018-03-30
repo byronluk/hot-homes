222858b6e7873abb3497fdaced405ee3c4e457c1
byronluk <byronluk@gmail.com>
2018-03-29 21:43:31 -0700

    testing heroku deploy

--- server/datasources.production.js
+++ server/datasources.production.js
@@ -2,6 +2,6 @@ module.exports = {
   MongoDB: {
     name: 'MongoDB',
     connector: 'loopback-connector-mongodb',
-    url: 'mongodb://byronluk:shotman1@ds121665.mlab.com:21665/heroku_60lmcp8p'
+    url: process.env.MONGODB_URI
   }
 };
