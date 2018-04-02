0013977216dbbe0f2991a2659b64d543bccf3e64
byronluk <byronluk@gmail.com>
2018-04-02 09:14:41 -0700

    updated production config

--- server/datasources.production.js
+++ server/datasources.production.js
@@ -2,11 +2,6 @@ module.exports = {
   MongoDB: {
     name: 'MongoDB',
     connector: 'loopback-connector-mongodb',
-<<<<<<< HEAD
     url: process.env.MONGODB_URI
-=======
-    url:
-      'mongodb://heroku_447q7njv:7r84ftus584hjjur0as7d19gne@ds021922.mlab.com:21922/heroku_447q7njv'
->>>>>>> 59b036e4cacad339c4571e7ed14bfa86c1f3bfa1
   }
 };
